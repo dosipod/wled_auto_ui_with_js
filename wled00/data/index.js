402ea80d356184849f1e061e4c9d81f94a2b03cd
AlDIY <87589371+dosipod@users.noreply.github.com>
2024-01-19 03:59:20 +0300

    Update index.js

--- wled00/data/index.js
+++ wled00/data/index.js
@@ -725,17 +725,17 @@ function populateInfo(i)
 ${urows}
 ${urows===""?'':'<tr><td colspan=2><hr style="height:1px;border-width:0;color:gray;background-color:gray"></td></tr>'}
 ${i.opt&0x100?inforow("Debug","<button class=\"btn btn-xs\" onclick=\"requestJson({'debug':"+(i.opt&0x0080?"false":"true")+"});\"><i class=\"icons "+(i.opt&0x0080?"on":"off")+"\">&#xe08f;</i></button>"):''}
-${inforow("Build",i.vid)}
-${inforow("Signal strength",i.wifi.signal +"% ("+ i.wifi.rssi, " dBm)")}
-${inforow("Uptime",getRuntimeStr(i.uptime))}
-${inforow("Time",i.time)}
-${inforow("Free heap",heap," kB")}
+${inforow("Build it",i.vid)}
+${inforow("Signal me",i.wifi.signal +"% ("+ i.wifi.rssi, " dBm)")}
+${inforow("UpDog",getRuntimeStr(i.uptime))}
+${inforow("Shor time?",i.time)}
+${inforow("Free men",heap," kB")}
 ${i.psram?inforow("Free PSRAM",(i.psram/1024).toFixed(1)," kB"):""}
-${inforow("Estimated current",pwru)}
-${inforow("Average FPS",i.leds.fps)}
-${inforow("MAC address",i.mac)}
-${inforow("Filesystem",i.fs.u + "/" + i.fs.t + " kB (" +Math.round(i.fs.u*100/i.fs.t) + "%)")}
-${inforow("Environment",i.arch + " " + i.core + " (" + i.lwip + ")")}
+${inforow("Stay current",pwru)}
+${inforow("Nice LFPS",i.leds.fps)}
+${inforow("MAC Less",o.ip)}
+${inforow("FSSSSS",i.fs.u + "/" + i.fs.t + " kB (" +Math.round(i.fs.u*100/i.fs.t) + "%)")}
+${inforow("Envery is bad",i.arch + " " + i.core + " (" + i.lwip + ")")}
 </table>`;
 	gId('kv').innerHTML = cn;
 	//  update all sliders in Info
